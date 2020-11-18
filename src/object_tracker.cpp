c283940b811ff53b9b2df2991da7f0423c93dca8
Sangmin <krsml90@gmail.com>
2020-11-18 16:53:16 +0900

    create simple android log and initialize object tracker functionality

--- src/object_tracker.cpp
+++ src/object_tracker.cpp
@@ -1,2 +1,37 @@
 #include "object_tracker.h"
+#include <unordered_map>
+#include <string>
+#include "simpleAndroidLog.h"
 
+VkResult ObjectTracker::PostCallAllocateMemory(VkDevice device, const VkMemoryAllocateInfo *pAllocateInfo, const VkAllocationCallbacks *pAllocator, VkDeviceMemory *pMemory, VkResult result)
+{
+    if(result == VK_SUCCESS)
+    {
+        totalAllocSize+=pAllocateInfo->allocationSize;
+        memorySizeMap[*pMemory] = pAllocateInfo->allocationSize;
+    }
+    return result;
+};
+
+void ObjectTracker::PreCallFreeMemory(VkDevice device, VkDeviceMemory memory, const VkAllocationCallbacks *pAllocator)
+{
+    if (memory != VK_NULL_HANDLE)
+    {
+        totalAllocSize -= memorySizeMap[memory];
+    }
+}
+
+VkResult ObjectTracker::PreCallQueuePresentKHR(VkQueue queue, const VkPresentInfoKHR *pPresentInfo)
+{
+    numFrame++;
+    if (numFrame >= tick)
+    {
+        numFrame = 0;
+        std::string info = "Alloc Memory: " + std::to_string(totalAllocSize);
+        LOGI("%s", info.c_str());
+    }
+    return VK_SUCCESS;
+};
+
+
+ObjectTracker objTracker;
--- src/object_tracker.h
+++ src/object_tracker.h
@@ -5,27 +5,20 @@ class ObjectTracker : public layer_factory
 {
 public:
 	ObjectTracker()
-		: totalAllocSize(0){};
-
-	VkResult PostCallAllocateMemory(VkDevice device, const VkMemoryAllocateInfo *pAllocateInfo, const VkAllocationCallbacks *pAllocator, VkDeviceMemory *pMemory, VkResult result)
-	{
-		if(result == VK_SUCCESS)
+		: totalAllocSize(0)
+		, numFrame(0)
 		{
-			totalAllocSize+=pAllocateInfo->allocationSize;
-			memorySizeMap[*pMemory] = pAllocateInfo->allocationSize;
-		}
-		return result;
-	};
+		};
 
-	void PreCallFreeMemory(VkDevice device, VkDeviceMemory memory, const VkAllocationCallbacks *pAllocator)
-	{
-		if (memory != VK_NULL_HANDLE)
-		{
-			totalAllocSize -= memorySizeMap[memory];
-		}
-	}
+	VkResult PostCallAllocateMemory(VkDevice device, const VkMemoryAllocateInfo *pAllocateInfo, const VkAllocationCallbacks *pAllocator, VkDeviceMemory *pMemory, VkResult result);
+
+	void PreCallFreeMemory(VkDevice device, VkDeviceMemory memory, const VkAllocationCallbacks *pAllocator);
+
+	VkResult PreCallQueuePresentKHR(VkQueue queue, const VkPresentInfoKHR *pPresentInfo);
 
 private:
 	size_t totalAllocSize;
+	size_t numFrame;
+	const size_t tick = 60;
 	std::unordered_map<VkDeviceMemory, VkDeviceSize> memorySizeMap;
 };
